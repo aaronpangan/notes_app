065d2fe6429480c80710fa766d1cdf7e639b0cd0
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-12-28 20:42:04 +0800

    Initialize App

---
+++ lib/ui/Home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Notes extends StatefulWidget {
+  @override
+  _NotesState createState() => _NotesState();
+}
+
+class _NotesState extends State<Notes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
