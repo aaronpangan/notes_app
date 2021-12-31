a8732ed39332fbd7edac36ec66b10248dad7dd47
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-12-31 12:37:19 +0800

    add basic auth with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/ui/Home.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,6 +9,71 @@ void main() async {
   await Firebase.initializeApp();
 
   runApp(MaterialApp(
-    home: Notes(),
-  ));
+      // home: Notes(),
+      home: Auth()));
+}
+
+class Auth extends StatefulWidget {
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${user == null ? "OUT" : user.email}"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+            ),
+            TextField(
+              controller: password,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () => _signUp(), child: Text("Sign UP")),
+                ElevatedButton(
+                    onPressed: () => _signIn(), child: Text("Sign IN")),
+                ElevatedButton(
+                    onPressed: () => _logout(), child: Text("Log Out"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _signUp() async {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: email.text, password: password.text)
+        .then((value) => print("Sign Up success"));
+    setState(() {});
+  }
+
+  _signIn() async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email.text, password: password.text)
+        .then((value) => print("Login SUccess"));
+    setState(() {});
+  }
+
+  _logout() async {
+    await FirebaseAuth.instance.signOut();
+    setState(() {});
+  }
 }
